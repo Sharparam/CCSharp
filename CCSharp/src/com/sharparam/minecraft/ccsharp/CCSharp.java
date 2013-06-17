6ee4f2599c14076056bfc50b4a035f144cf0cbc0
Adam Hellberg <adam.hellberg95@gmail.com>
2013-06-18 01:52:32 +0200

    Initial commit

---
+++ CCSharp/src/com/sharparam/minecraft/ccsharp/CCSharp.java
@@ -0,0 +1,74 @@
+/*
+ * CCSharp.java
+ *
+ * Copyright © 2013 by Adam Hellberg <adam.hellberg@sharparam.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.sharparam.minecraft.ccsharp;
+
+import cpw.mods.fml.common.FMLLog;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+
+import java.util.logging.Logger;
+
+/**
+ * User: Sharparam
+ * Date: 2013-06-17
+ * Time: 19:37
+ */
+@Mod(modid = CCSharp.ID, name = CCSharp.NAME, version = CCSharp.VERSION)
+@NetworkMod(clientSideRequired = true, serverSideRequired = false)
+public class CCSharp {
+    public static final String ID = "CCSharp";
+    public static final String NAME = "CCSharp";
+    public static final String VERSION = "0.0.0";
+
+    @Mod.Instance(ID)
+    public static CCSharp instance;
+
+    private Logger log;
+
+    @SidedProxy(
+            clientSide = "com.sharparam.minecraft.ccsharp.client.ClientProxy",
+            serverSide = "com.sharparam.minecraft.ccsharp.server.ServerProxy")
+    public static IProxy proxy;
+
+    @Mod.PreInit
+    public void preInit(FMLPreInitializationEvent event) {
+        log = Logger.getLogger(ID);
+        log.setParent(FMLLog.getLogger());
+    }
+
+    @Mod.Init
+    public void init(FMLInitializationEvent event) {
+        proxy.registerRenderers();
+    }
+
+    @Mod.PostInit
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+}
---
+++ CCSharp/src/com/sharparam/minecraft/ccsharp/client/ClientProxy.java
@@ -0,0 +1,42 @@
+/*
+ * ClientProxy.java
+ *
+ * Copyright © 2013 by Adam Hellberg <adam.hellberg@sharparam.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.sharparam.minecraft.ccsharp.client;
+
+import com.sharparam.minecraft.ccsharp.IProxy;
+import net.minecraftforge.client.MinecraftForgeClient;
+
+/**
+ * User: Sharparam
+ * Date: 2013-06-17
+ * Time: 19:40
+ */
+public class ClientProxy implements IProxy {
+    private static final String BLOCK_TEXTURE = "/com/sharparam/minecraft/ccsharp/block.png";
+
+    @Override
+    public void registerRenderers() {
+        MinecraftForgeClient.preloadTexture(BLOCK_TEXTURE);
+    }
+}
