a8b8efa6ba048837b710e2355e3cf2a88df8e791
Adam Hellberg <sharparam@sharparam.com>
2013-06-23 06:52:31 +0200

    Version update

--- CCSharp/src/main/java/com/sharparam/minecraft/ccsharp/CCSharp.java
+++ CCSharp/src/main/java/com/sharparam/minecraft/ccsharp/CCSharp.java
@@ -50,7 +50,7 @@ import java.util.logging.Logger;
 public class CCSharp {
     public static final String ID = "CCSharp";
     public static final String NAME = "CCSharp";
-    public static final String VERSION = "0.0.14";
+    public static final String VERSION = "0.0.15";
 
     @Mod.Instance(ID)
     public static CCSharp instance;
